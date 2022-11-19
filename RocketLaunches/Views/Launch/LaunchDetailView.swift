2f4874548fb87ed0906cc47f9be9b00b85719b64
Tim Mitra <tim@it-guy.com>
2022-11-19 01:38:02 -0500

    detail vies

--- RocketLaunches/Views/Launch/LaunchDetailView.swift
+++ RocketLaunches/Views/Launch/LaunchDetailView.swift
@@ -3,15 +3,54 @@
 import SwiftUI
 
 struct LaunchDetailView: View {
+  @Environment(\.managedObjectContext) var viewContext
+  let launch: RocketLaunch
+  let launchName: String?
+  let launchNotes: String?
+  let launchDate: Date?
+  let isViewed: Bool?
+
+  init(launch: RocketLaunch) {
+    self.launch = launch
+    self.launchName = launch.name
+    self.launchNotes = launch.notes
+    self.launchDate = launch.launchDate
+    self.isViewed = launch.isViewed
+  }
+
   var body: some View {
-    Text("Launch details go here")
-    .navigationTitle("Sample Launch")
+    Form {
+      Section("Notes") {
+        Text(launchNotes ?? "No Notes")
+      }
+      if let launchDate = launchDate {
+        Section("Launch Date") {
+          Text(launchDate.formatted())
+        }
+      }
+      Section {
+        Button("Mark as Viewed") {
+          self.launch.isViewed = true
+          do {
+            try viewContext.save()
+          } catch {
+            print("Error marking launch as viewed \(error)")
+          }
+        }
+      }
+    }
+    .navigationTitle(self.launchName ?? "Sample Launch")
     .background(Color(.systemGroupedBackground))
   }
 }
 
-struct LaunchDetailView_Previews: PreviewProvider {
-  static var previews: some View {
-    LaunchDetailView()
-  }
-}
+//struct LaunchDetailView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    let context = PersistenceController.preview.container.viewContext
+//    let newLaunch = RocketLaunch(context: context)
+//    newLaunch.name = "Some launch"
+//    newLaunch.notes = "Here are the notes"
+//    newLaunch.launchDate = Date()
+//    return LaunchDetailView(launch: newLaunch)
+//  }
+//}
