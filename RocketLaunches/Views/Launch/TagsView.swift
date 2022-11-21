50b386faf6001f1a8ca558f20dcd99052546026e
Tim Mitra <tim@it-guy.com>
2022-11-20 23:43:18 -0500

    fill out TagsView

--- RocketLaunches/Views/Launch/TagsView.swift
+++ RocketLaunches/Views/Launch/TagsView.swift
@@ -3,13 +3,31 @@
 import SwiftUI
 
 struct TagsView: View {
+  // access the keypath to the moc with the Environment Object
+  @Environment(\.managedObjectContext) var viewContext
+  let tags: [Tag]
+  
     var body: some View {
-        Text("Hello, World!")
+      NavigationView {
+        VStack {
+          List {
+            Section {
+              ForEach(tags, id: \.self) { tag in
+                Text(tag.title ?? "")
+              }
+            }
+          }
+        }
+      }
+      .navigationTitle(Text("Tags"))
     }
 }
 
 struct TagsView_Previews: PreviewProvider {
     static var previews: some View {
-        TagsView()
+      let context = PersistenceContoller.preview.container.viewContext
+      let tag = Tag(context: context)
+      tag.title = "preview tag"
+      return TagsView(tags:[tag])
     }
 }
