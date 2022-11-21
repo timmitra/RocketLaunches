2761d56801d0aa389a0010a9cea08ef834cd9767
Tim Mitra <tim@it-guy.com>
2022-11-20 23:24:06 -0500

    add TagsView

---
+++ RocketLaunches/Views/Launch/TagsView.swift
@@ -0,0 +1,15 @@
+/// Copyright (c) 2022 Razeware LLC
+
+import SwiftUI
+
+struct TagsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TagsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagsView()
+    }
+}
